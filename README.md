f5e19f668ec2b1750f64b7384b749525f1c613a4
Unlocking the future code <live@unlocking.com>
2018-06-14 15:53:07 +0700

    Initialize repository.
    Signed-off-by: Unlocking the future code <live@unlocking.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# unlocking-ep15
+# Unlocking-EP 15
+
+ตัวอย่าง source code การทำ Web API (asp.net core mvc) โดยการรับส่งข้อมูลเป็น model
---
+++ demoapi/Controllers/MyApiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using demoapi.Models;
+
+namespace demoapi.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class MyApiController : Controller
+    {
+        [HttpGet]
+        public string Greeting()
+        {
+            return "Hello world";
+        }
+
+        [HttpGet("{name}")]
+        public string Greeting(string name)
+        {
+            return "Hello " + name;
+        }
+
+        [HttpPost("{productName}")]
+        public string CreateNewProduct(string productName)
+        {
+            // เพิ่มสินค้าใหม่ในระบบ
+            return "Done";
+        }
+
+        [HttpPut("{id}/{name}")]
+        public string UpdateProductName(string id, string name)
+        {
+            // แก้ไขชื่อสินค้าที่เลือกจากรหัสสินค้า
+            return "Update product completed.";
+        }
+    }
+}
